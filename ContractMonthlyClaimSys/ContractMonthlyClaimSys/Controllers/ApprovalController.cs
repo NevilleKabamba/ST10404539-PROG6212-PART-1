76a346623ab696c19dc31282ffa9ede5295cc36a
NevilleKabamba <st10404539@vcconnect.edu.za>
2024-09-10 09:43:43 +0200

    Create Approval Controller

---
+++ ContractMonthlyClaimSys/ContractMonthlyClaimSys/Controllers/ApprovalController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSys.Controllers
+{
+    // ApprovalController manages actions for approving claims
+    public class ApprovalController : Controller
+    {
+        // Action method to display the 'Approve Claims' page
+        public IActionResult ApproveClaims()
+        {
+            return View();
+        }
+    }
+}
