639c02870901b9ee06d08e6d399fb4922affe945
NevilleKabamba <st10404539@vcconnect.edu.za>
2024-09-10 09:49:54 +0200

    Create Lecturer Controller

---
+++ ContractMonthlyClaimSys/ContractMonthlyClaimSys/Controllers/LecturerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSys.Controllers
+{
+    // LecturerController handles actions for lecturers
+    public class LecturerController : Controller
+    {
+        // Action method to display the 'Submit Claim' form
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+        // Action method to display the 'Upload Documents' form
+        public IActionResult UploadDocuments()
+        {
+            return View();
+        }
+
+        // Action method to display the 'Check Status' page
+        public IActionResult CheckStatus()
+        {
+            return View();
+        }
+    }
+}
